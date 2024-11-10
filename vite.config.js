fccc78408f9e1d894d01e8b7ecb95cef5329e891
Ethan <ethanhedden0118@gmail.com>
2024-11-10 17:54:52 -0500

    Change vite config yet again

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import {defineConfig} from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: `/alice-activity-client/`,
+  base: `/`,
    envDir: '../',
     server: {
      proxy: {
