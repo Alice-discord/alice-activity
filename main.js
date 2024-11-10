8e1bbea9f8533efaa842803d8576f79f34287f38
Ethan <ethanhedden0118@gmail.com>
2024-11-09 23:46:45 -0500

    Please

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-import "./style.css";
-import rocketLogo from './rocket.png';
-
 document.querySelector('#app').innerHTML = `
   <div>
-    <img src="${rocketLogo}" class="logo" alt="Discord" />
+    <img src="./rocket.png" class="logo" alt="Discord" />
     <h1>Hello, World!</h1>
   </div>
 `;
