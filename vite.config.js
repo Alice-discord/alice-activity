714aedb1ed5995fe8e88cf93089565ce2d310e82
Ethan <ethanhedden0118@gmail.com>
2024-11-10 18:07:12 -0500

    Commit to gh please

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import {defineConfig} from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: `/`,
+  base: `/alice-activity-client`,
    envDir: '../',
     server: {
      proxy: {
