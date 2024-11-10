99f5e2838464291d02e3421992084317fbccced0
Ethan <ethanhedden0118@gmail.com>
2024-11-09 22:13:07 -0500

    Third commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/alice-activity-client/", // YOUR REPO NAME HERE
+  base: "/alice-activity-client/client/", // YOUR REPO NAME HERE
    envDir: '../',
     server: {
      proxy: {
