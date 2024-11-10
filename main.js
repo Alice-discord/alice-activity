2f3777836cd9ed6cd54ad0c12daeda053d01cdc1
Ethan <ethanhedden0118@gmail.com>
2024-11-09 23:51:03 -0500

    Add discord sdk back

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+// Import the SDK
+import { DiscordSDK } from "@discord/embedded-app-sdk";
+
+// Instantiate the SDK
+const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+
+setupDiscordSdk().then(() => {
+  console.log("Discord SDK is ready");
+});
+
+async function setupDiscordSdk() {
+  await discordSdk.ready();
+}
+
+
 document.querySelector('#app').innerHTML = `
   <div>
     <img src="./rocket.png" class="logo" alt="Discord" />
