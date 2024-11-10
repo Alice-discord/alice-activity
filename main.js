36b203fc35c6fd59056343323d56aeb02b907497
Ethan <ethanhedden0118@gmail.com>
2024-11-10 17:53:07 -0500

    Revert back to discord guide

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
-import rocketLogo from '/rocket.png';
-
 // Import the SDK
-import { DiscordSDK } from "./node_modules/@discord/embedded-app-sdk";
+import { DiscordSDK } from "@discord/embedded-app-sdk";
+
+import "./style.css";
+import rocketLogo from '/rocket.png';
 
 // Instantiate the SDK
 const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
-
-async function setupDiscordSdk() {
-  await discordSdk.ready();
-}
 
 setupDiscordSdk().then(() => {
   console.log("Discord SDK is ready");
 });
 
-document.querySelector('#html').innerHTML = `
+async function setupDiscordSdk() {
+  await discordSdk.ready();
+}
+
+document.querySelector('#app').innerHTML = `
   <div>
     <img src="${rocketLogo}" class="logo" alt="Discord" />
     <h1>Hello, World!</h1>
