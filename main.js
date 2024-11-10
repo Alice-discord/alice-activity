2d7ee501f7f62bca13cd74bbe6cbb34804c7053f
Ethan <ethanhedden0118@gmail.com>
2024-11-10 00:30:14 -0500

    Yet another commit to test gh pages

--- main.js
+++ main.js
@@ -1,18 +1,4 @@
-// Import the SDK
-import { DiscordSDK } from "@discord/embedded-app-sdk";
-
 import rocketLogo from '/rocket.png';
-
-// Instantiate the SDK
-const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
-
-async function setupDiscordSdk() {
-  await discordSdk.ready();
-}
-
-setupDiscordSdk().then(() => {
-  console.log("Discord SDK is ready");
-});
 
 document.querySelector('#html').innerHTML = `
   <div>
