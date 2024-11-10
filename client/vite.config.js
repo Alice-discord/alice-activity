8907425cf7d586847b9f6c371a6da7aed8661626
Ethan <ethanhedden0118@gmail.com>
2024-11-09 22:12:01 -0500

    Second commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,13 @@
 import {defineConfig} from 'vite';
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: '../',
-  server: {
-    proxy: {
+  plugins: [react()],
+  base: "/alice-activity-client/", // YOUR REPO NAME HERE
+   envDir: '../',
+    server: {
+     proxy: {
       '/api': {
         target: 'http://localhost:3001',
         changeOrigin: true,
