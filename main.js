c8cfe1afb97ed6fa13a12b873311563c529fe89d
Ethan <ethanhedden0118@gmail.com>
2024-11-10 00:35:55 -0500

    Why i outghta

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 import rocketLogo from '/rocket.png';
 
+// Import the SDK
+import { DiscordSDK } from "@discord/embedded-app-sdk";
+
+// Instantiate the SDK
+const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+
+async function setupDiscordSdk() {
+  await discordSdk.ready();
+}
+
+setupDiscordSdk().then(() => {
+  console.log("Discord SDK is ready");
+});
+
 document.querySelector('#html').innerHTML = `
   <div>
     <img src="${rocketLogo}" class="logo" alt="Discord" />
