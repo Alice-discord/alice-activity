029248091a72088e5bc429b84b17b54944cbb871
Ethan <ethanhedden0118@gmail.com>
2024-11-09 22:46:59 -0500

    Add back discord sdk

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 import "./style.css";
 import rocketLogo from '/rocket.png';
 
+// Import the SDK
+import { DiscordSDK } from "@discord/embedded-app-sdk";
+
+// Instantiate the SDK
+const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+
+setupDiscordSdk().then(() => {
+  console.log("Discord SDK is ready");
+});
+
+async function setupDiscordSdk() {
+  await discordSdk.ready();
+}
+
 document.querySelector('#app').innerHTML = `
   <div>
     <img src="${rocketLogo}" class="logo" alt="Discord" />
