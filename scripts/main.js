c9c481a0a8ef21e538894c7d16a4c42f466f2942
Ethan <ethanhedden0118@gmail.com>
2024-11-12 11:39:38 -0500

    Commit

--- scripts/main.js
+++ scripts/main.js
@@ -34,29 +34,7 @@ async function setupDiscordSdk() {
     ],
   });
 
-  // Retrieve an access_token from your activity's server
-  // Note: We need to prefix our backend `/api/token` route with `/.proxy` to stay compliant with the CSP.
-  // Read more about constructing a full URL and using external resources at
-  // https://discord.com/developers/docs/activities/development-guides#construct-a-full-url
-  const response = await fetch("/server/.proxy/api/token", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      code,
-    }),
-  });
-  const { access_token } = await response.json();
-
-  // Authenticate with Discord client (using the access_token)
-  auth = await discordSdk.commands.authenticate({
-    access_token,
-  });
-
-  if (auth == null) {
-    throw new Error("Authenticate command failed");
-  }
+ 
 }
 
 document.querySelector('#app').innerHTML = `
