208b0e89066263a135c46f52684ef5fa59e43df2
Ethan <ethanhedden0118@gmail.com>
2024-11-12 09:22:24 -0500

    Test if this works

--- scripts/main.js
+++ scripts/main.js
@@ -5,17 +5,24 @@ import { DiscordSDK } from "@discord/embedded-app-sdk";
 // Define varibles (For the life of me i cant get .env varibles working)
 const DISCORD_CLIENT_ID = `1303887717247090758`
 
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Initializing the discord sdk...
+    <p>This is an discord activity to be used with discord!!
+  </div>
+`;
+
 if (!import.meta.env.DEV) {
-// Instantiate the SDK
-const discordSdk = new DiscordSDK(DISCORD_CLIENT_ID);
+  // Instantiate the SDK
+  const discordSdk = new DiscordSDK(DISCORD_CLIENT_ID);
 
-async function setupDiscordSdk() {
-  await discordSdk.ready();
-}
+  async function setupDiscordSdk() {
+    await discordSdk.ready();
+  }
 
-setupDiscordSdk().then(() => {
-  console.log("Discord SDK is ready");
-});
+  setupDiscordSdk().then(() => {
+    console.log("Discord SDK is ready");
+  });
 }
 
 document.querySelector('#app').innerHTML = `
