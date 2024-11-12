375d46cb355f06a5d9e993855a3df9a9826dea5b
Ethan <ethanhedden0118@gmail.com>
2024-11-11 21:30:03 -0500

    Commit

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
 import "/style.css";
-import rocketLogo from '/rocket.png';
-
-// Import the SDK
 import { DiscordSDK } from "@discord/embedded-app-sdk";
+// Define varibles (For the life of me i cant get .env varibles working)
+const devmode = true
 const DISCORD_CLIENT_ID = `1303887717247090758`
+
+if (!devmode) {
 
 // Instantiate the SDK
 const discordSdk = new DiscordSDK(DISCORD_CLIENT_ID);
 
-setupDiscordSdk().then(() => {
-  console.log("Discord SDK is ready");
-});
 
 async function setupDiscordSdk() {
   await discordSdk.ready();
 }
 
+setupDiscordSdk().then(() => {
+  console.log("Discord SDK is ready");
+});
+}
+
 document.querySelector('#app').innerHTML = `
   <div>
-    <img src="${rocketLogo}" class="logo" alt="Discord" />
     <h1>Hello, World!</h1>
-    <p>This is all temporary!!
   </div>
 `;
