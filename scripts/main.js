675f172c96df72e6d97f836dfe9752022a75d143
Ethan <ethanhedden0118@gmail.com>
2024-11-12 13:39:42 -0500

    why gh pages 🤬

--- scripts/main.js
+++ scripts/main.js
@@ -5,31 +5,30 @@ import { DiscordSDK } from "@discord/embedded-app-sdk";
 // Define varibles (For the life of me i cant get .env varibles working)
 const DISCORD_CLIENT_ID = `1303887717247090758`
 
-if (!import.meta.env.DEV) {
-  let auth;
+let auth;
 
-  let serverurl = new URL(`http://${DISCORD_CLIENT_ID}.discordsays.com/.proxy/server/alicediscord`);
+let serverurl = new URL(`http://${DISCORD_CLIENT_ID}.discordsays.com/.proxy/server/alicediscord`);
 
-  const discordSdk = new DiscordSDK(DISCORD_CLIENT_ID);
+const discordSdk = new DiscordSDK(DISCORD_CLIENT_ID);
   
-  async function setupDiscordSdk() {
-    await discordSdk.ready();
-    console.log("Discord SDK is ready");
+async function setupDiscordSdk() {
+  await discordSdk.ready();
+  console.log("Discord SDK is ready");
 
-    // Authorize with Discord Client
-    const { code } = await discordSdk.commands.authorize({
-      client_id: DISCORD_CLIENT_ID,
-      response_type: "code",
-      state: "",
-      prompt: "none",
-      scope: [
-        "identify",
-        "guilds",
-        "applications.commands"
-      ],
-    });
+// Authorize with Discord Client
+const { code } = await discordSdk.commands.authorize({
+  client_id: DISCORD_CLIENT_ID,
+    response_type: "code",
+    state: "",
+    prompt: "none",
+    scope: [
+      "identify",
+      "guilds",
+      "applications.commands"
+    ],
+  });
   
-    // Retrieve an access_token from your activity's server
+  // Retrieve an access_token from your activity's server
     // Note: We need to prefix our backend `/api/token` route with `/.proxy` to stay compliant with the CSP.
     // Read more about constructing a full URL and using external resources at
     // https://discord.com/developers/docs/activities/development-guides#construct-a-full-url
@@ -55,37 +54,36 @@ if (!import.meta.env.DEV) {
   }
 
   async function appendVoiceChannelName() {
-    const app = document.querySelector('#app');
+const app = document.querySelector('#app');
   
-    let activityChannelName = 'Unknown';
+  let activityChannelName = 'Unknown';
   
-    // Requesting the channel in GDMs (when the guild ID is null) requires
-    // the dm_channels.read scope which requires Discord approval.
-    if (discordSdk.channelId != null && discordSdk.guildId != null) {
-      // Over RPC collect info about the channel
-      const channel = await discordSdk.commands.getChannel({channel_id: discordSdk.channelId});
-      if (channel.name != null) {
-        activityChannelName = channel.name;
-      }
+  // Requesting the channel in GDMs (when the guild ID is null) requires
+  // the dm_channels.read scope which requires Discord approval.
+  if (discordSdk.channelId != null && discordSdk.guildId != null) {
+    // Over RPC collect info about the channel
+    const channel = await discordSdk.commands.getChannel({channel_id: discordSdk.channelId});
+    if (channel.name != null) {
+      activityChannelName = channel.name;
     }
-  
-    // Update the UI with the name of the current voice channel
-    const textTagString = `Activity Channel: "${activityChannelName}"`;
-    const textTag = document.createElement('p');
-    textTag.textContent = textTagString;
-    app.appendChild(textTag);
   }
   
-  setupDiscordSdk().then(() => {
-    console.log("Discord SDK is authenticated");
-  
-    // We can now make API calls within the scopes we requested in setupDiscordSDK()
-    // Note: the access_token returned is a sensitive secret and should be treated as such
-    appendVoiceChannelName();
-  });
-
+  // Update the UI with the name of the current voice channel
+  const textTagString = `Activity Channel: "${activityChannelName}"`;
+  const textTag = document.createElement('p');
+  textTag.textContent = textTagString;
+  app.appendChild(textTag);
 }
 
+
+
+setupDiscordSdk().then(() => {
+  console.log("Discord SDK is authenticated");
+
+  // We can now make API calls within the scopes we requested in setupDiscordSDK()
+  // Note: the access_token returned is a sensitive secret and should be treated as such
+  appendVoiceChannelName();
+});
 
 document.querySelector('#app').innerHTML = `
   <div>
