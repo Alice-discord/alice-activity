c45d033bf71e697d8c8a308d83316bcf222eb2e0
Ethan <ethanhedden0118@gmail.com>
2024-11-12 12:07:01 -0500

    Revert to a usable state

--- scripts/main.js
+++ scripts/main.js
@@ -5,36 +5,17 @@ import { DiscordSDK } from "@discord/embedded-app-sdk";
 // Define varibles (For the life of me i cant get .env varibles working)
 const DISCORD_CLIENT_ID = `1303887717247090758`
 
-// Will eventually store the authenticated user's access_token
-let auth;
+if (!import.meta.env.DEV) {
+  // Instantiate the SDK
+  const discordSdk = new DiscordSDK(DISCORD_CLIENT_ID);
 
-const discordSdk = new DiscordSDK(DISCORD_CLIENT_ID);
+  async function setupDiscordSdk() {
+    await discordSdk.ready();
+  }
 
-setupDiscordSdk().then(() => {
-  console.log("Discord SDK is authenticated");
-
-  // We can now make API calls within the scopes we requested in setupDiscordSDK()
-  // Note: the access_token returned is a sensitive secret and should be treated as such
-});
-
-async function setupDiscordSdk() {
-  await discordSdk.ready();
-  console.log("Discord SDK is ready");
-
-  // Authorize with Discord Client
-  const { code } = await discordSdk.commands.authorize({
-    client_id: DISCORD_CLIENT_ID,
-    response_type: "code",
-    state: "",
-    prompt: "none",
-    scope: [
-      "identify",
-      "guilds",
-      "applications.commands"
-    ],
+  setupDiscordSdk().then(() => {
+    console.log("Discord SDK is ready");
   });
-
- 
 }
 
 document.querySelector('#app').innerHTML = `
