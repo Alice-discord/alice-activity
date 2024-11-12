4382d99c776a9875b84fc553a1d95ddd817af65c
Ethan <ethanhedden0118@gmail.com>
2024-11-12 12:54:24 -0500

    Try this? maybe?

--- scripts/main.js
+++ scripts/main.js
@@ -6,16 +6,58 @@ import { DiscordSDK } from "@discord/embedded-app-sdk";
 const DISCORD_CLIENT_ID = `1303887717247090758`
 
 if (!import.meta.env.DEV) {
-  // Instantiate the SDK
-  const discordSdk = new DiscordSDK(DISCORD_CLIENT_ID);
+  let auth;
 
+  const discordSdk = new DiscordSDK(DISCORD_CLIENT_ID);
+  
+  setupDiscordSdk().then(() => {
+    console.log("Discord SDK is authenticated");
+  
+    // We can now make API calls within the scopes we requested in setupDiscordSDK()
+    // Note: the access_token returned is a sensitive secret and should be treated as such
+  });
+  
   async function setupDiscordSdk() {
     await discordSdk.ready();
-  }
-
-  setupDiscordSdk().then(() => {
     console.log("Discord SDK is ready");
-  });
+  
+    // Authorize with Discord Client
+    const { code } = await discordSdk.commands.authorize({
+      client_id: DISCORD_CLIENT_ID,
+      response_type: "code",
+      state: "",
+      prompt: "none",
+      scope: [
+        "identify",
+        "guilds",
+        "applications.commands"
+      ],
+    });
+  
+    // Retrieve an access_token from your activity's server
+    // Note: We need to prefix our backend `/api/token` route with `/.proxy` to stay compliant with the CSP.
+    // Read more about constructing a full URL and using external resources at
+    // https://discord.com/developers/docs/activities/development-guides#construct-a-full-url
+    const response = await fetch("/server/alicediscord/.proxy/api/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        code,
+      }),
+    });
+    const { access_token } = await response.json();
+  
+    // Authenticate with Discord client (using the access_token)
+    auth = await discordSdk.commands.authenticate({
+      access_token,
+    });
+  
+    if (auth == null) {
+      throw new Error("Authenticate command failed");
+    }
+  }
 }
 
 document.querySelector('#app').innerHTML = `
